0fc6c7b3cc6ba673aee9a56c9372cb301857938e
daliawk <63318701+daliawk@users.noreply.github.com>
2020-10-28 20:21:11 +0200

    Comined all work in main

--- Minterms.h
+++ Minterms.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef MINTERMS
+#define MINTERMS
 #include <iostream>
 #include <vector>
 
@@ -8,14 +9,58 @@ using namespace std;
 class Minterms {
 private:
 	vector<int> values;
-	int digits;
+	vector<char> digits;
+	int numOnes;
+	int size;
 	bool minterm;
 public:
-	Minterms(int value, bool min );
+	Minterms() { numOnes = 0; size = 0; }
+	Minterms(int value, bool min, int s);
+	void SetUp(int value, bool min, int s);
+	void decToBinary();
+	int NumberOnes() { return numOnes; }
+	void printBinary() { for (int i = 0; i < size; i++) cout << digits[i]; }
 };
 
 
-Minterms::Minterms(int value, bool min) {
+Minterms::Minterms(int value, bool min, int s) {
 	values.push_back(value);
 	minterm = min;
+	size = s;
+	digits.resize(s);
 }
+
+void Minterms::SetUp(int value, bool min, int s) {
+	values.push_back(value);
+	minterm = min;
+	size = s;
+	digits.resize(s);
+	decToBinary();
+
+	numOnes;
+	for (int i = 0; i < size; i++) {
+		if (digits[i] == 0) {
+			numOnes++;
+		}
+	}
+}
+
+void Minterms::decToBinary()
+{
+	int n = values[0];
+
+	int i = 0;
+	while (n > 0) {
+
+		digits[i] = n % 2;
+		n = n / 2;
+		i++;
+	}
+
+	cout << endl;
+	for (int j = i - 1; j >= 0; j--)
+		cout << digits[j];
+	cout << endl;
+}
+
+#endif
